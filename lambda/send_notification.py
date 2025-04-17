08a1a6000094e954c8c31ff9c1db40b962fc1711
Divij Ganjoo <divijganjoo2003@gmail.com>
2025-04-17 17:11:57 +0530

    Update send_notification.py

--- lambda/send_notification.py
+++ lambda/send_notification.py
@@ -1,1 +1,13 @@
+import boto3
 
+def lambda_handler(event, context):
+    sns = boto3.client("sns")
+    response = sns.publish(
+        TopicArn="arn:aws:sns:region:account-id:your-topic",
+        Message=event.get("message", "Hello from SkillShare!"),
+        Subject="SkillShare Notification"
+    )
+    return {
+        "statusCode": 200,
+        "messageId": response["MessageId"]
+    }
