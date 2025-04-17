460643f7c5e17b8cac7fb9cfe1cb840f336b5621
Divij Ganjoo <divijganjoo2003@gmail.com>
2025-04-17 17:32:57 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,10 @@
+const { v4: uuidv4 } = require('uuid');
+
+const createUser = async (req, res) => {
+  const user = { ...req.body, id: uuidv4() };
+  // Save to DynamoDB
+  res.status(201).json({ message: 'User registered', user });
+};
+
+module.exports = { createUser };
 
