844238414ca8cfec3c7238a91f55dc0ab3fc3a6e
Divij Ganjoo <divijganjoo2003@gmail.com>
2025-04-17 17:11:39 +0530

    Update process_payment.py

--- lambda/process_payment.py
+++ lambda/process_payment.py
@@ -1,1 +1,9 @@
-
+def lambda_handler(event, context):
+    user = event.get("user")
+    amount = event.get("amount")
+    
+    # Simulate payment processing
+    return {
+        "statusCode": 200,
+        "body": f"Payment of ${amount} processed for {user}"
+    }
