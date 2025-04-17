8ec90e102353091f09e9da732e21046c0017ee50
Divij Ganjoo <divijganjoo2003@gmail.com>
2025-04-17 17:29:07 +0530

    Update classes.js

--- backend/routes/classes.js
+++ backend/routes/classes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getAllClasses, createClass } = require('../controllers/classController');
 
+router.get('/', getAllClasses);
+router.post('/', createClass);
+
+module.exports = router;
